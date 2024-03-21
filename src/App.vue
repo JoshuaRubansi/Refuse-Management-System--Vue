a49b60ca0a7dc6bc4252355722df5cb14a5a9c91
Rubansi Joshua <joshuarubansi9@gmail.com>
2024-03-21 15:48:23 +0300

    First commit
    First commint: 3/21/2024

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+   <SideBar /> 
+  </div>
+  <div>
+    <ClientVue/>
+    <LoginVue />
+    <CoverPageVue />
+  </div>
+</template>
+
+<script setup>
+import ClientVue from './components/Client.vue';
+import CoverPageVue from './components/CoverPage.vue';
+import LoginVue from './components/Login.vue';
+import Login from './components/Login.vue';
+import SideBar from './components/SideBar.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="relative flex min-h-screen">
+    <!-- Sidebar -->
+    <div class=" bg-cyan-600 text-cyan-100 w-64 space-y-6 px-2"><a href="" class="flex items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="yellow" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+</svg>
+<span class="font-extrabold text-2xl text-white ">Recycle</span>
+</a>
+<nav>
+  <a href="/Dashboard" class="flex items-center py-3 px-4 hover:bg-cyan-800 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
+<span class="text-white font-semibold">Dashboard </span>
+</a>
+<a href="/Clients" class="flex items-center py-3 px-4 hover:bg-cyan-800 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>
+<span class="text-white font-semibold">Client </span>
+</a>
+<a href="/Customers" class="flex items-center py-3 px-4 hover:bg-cyan-800 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+</svg>
+<span class="text-white font-semibold hover:bg-cyan-800 rounded"
+@click="ToggleLogin">Customers </span>
+</a>
+<a href="/About" class="flex items-center py-3 px-4 hover:bg-cyan-800 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
+</svg>
+
+<span class="text-white font-semibold ">About </span>
+</a>
+</nav>
+
+</div>
+    <!-- main content -->
+    <div class="flex-1">
+      <TopBarVue />
+    </div>
+    
+
+  </div>
+</template>
+
+<script setup>
+import Client from './Client.vue';
+import TopBarVue from './TopBar.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
